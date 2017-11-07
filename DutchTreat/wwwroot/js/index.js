abd9e3d010256ff68ac05226b07d79c88e05996c
David Zorn <davidzorn@curriqunet.com>
2017-11-07 08:26:10 -0700

    Added jQuery and some usage

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,8 +1,27 @@
-﻿
-var x = 0;
-var s = "";
+﻿$(document).ready(function() {
+    var x = 0;
+    var s = "";
 
-console.log("Hello, World!");
+    console.log("Hello, World!");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        alert("Buying Item");
+    });
+
+    var productInfo = $(".product-properties li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(500);
+    });
+
+});
